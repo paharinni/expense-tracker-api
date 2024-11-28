4b7e7fbc0357bc064790c558ad5c1dda6090d1e6
paharinni <pakharenkobusiness@gmail.com>
2024-11-28 06:44:53 +0200

    Added ctor to ApplicationDbContext.cs

--- ExpenseTrackerApi/Data/ApplicationDbContext.cs
+++ ExpenseTrackerApi/Data/ApplicationDbContext.cs
@@ -5,6 +5,11 @@ namespace ExpenseTrackerApi.Data;
 
 public class ApplicationDbContext : DbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    
     public DbSet<User> Users { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
     public DbSet<Category> Categories { get; set; }
