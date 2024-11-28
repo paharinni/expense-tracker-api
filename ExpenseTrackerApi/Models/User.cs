80c0d29b6727a5238c28c0a84178e87254b4ca4f
paharinni <pakharenkobusiness@gmail.com>
2024-11-28 05:11:43 +0200

    Created User model

---
+++ ExpenseTrackerApi/Models/User.cs
@@ -0,0 +1,12 @@
+namespace ExpenseTrackerApi.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public int PhoneNumber { get; set; }
+    public int Email { get; set; }
+    public int PasswordHash { get; set; }
+}
