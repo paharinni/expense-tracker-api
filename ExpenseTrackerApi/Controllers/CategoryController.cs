187261232de613a4156f953d57169c0f1c866280
paharinni <pakharenkobusiness@gmail.com>
2024-11-30 05:57:36 +0200

    Created Category controller with CRUD operations.

---
+++ ExpenseTrackerApi/Controllers/CategoryController.cs
@@ -0,0 +1,102 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ExpenseTrackerApi.Data;
+using ExpenseTrackerApi.Models;
+
+namespace ExpenseTrackerApi.Controllers
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategoryByIdAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategoryAsync(int id, Category category)
+        {
+            var updatedCategory = await _context.Categories.FindAsync(id);
+            if (updatedCategory == null)
+            {
+                return NotFound();
+            }
+
+            updatedCategory.Name = category.Name;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> CreateCategoryAsync(Category category)
+        {
+            var createdCategory = new Category()
+            {
+                Name = category.Name,
+            };
+            
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return Ok(createdCategory);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategoryAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(c => c.Id == id);
+        }
+    }
+}
